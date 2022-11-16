48da7a93b1fb7e3092f216509cc2d53c9532675d
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2022-11-15 18:19:18 -0600

    creacion de triangulos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import struct
 from obj import Obj
+from Vector import *
 
 def char(c):
   return struct.pack('=c', c.encode('ascii'))
@@ -107,12 +108,22 @@ class Render(object):
   def set_color(self, color):
     self.current_color = color
     
-  def glLine(self,x1, y1, x2, y2):
-        x1 = int(round((x1+1) * self.width / 2))
-        y1 = int(round((y1+1) * self.height / 2))
-        x2 = int(round((x2+1) * self.width / 2))
-        y2 = int(round((y2+1) * self.height / 2))
-        steep=(abs(y2 - y1))>(abs(x2 - x1))
+  def glLine(self,v1,v2):
+        x1 = round(v1.x)
+        y1 = round(v1.y)
+        x2 = round(v2.x)
+        y2 = round(v2.y)
+        
+        dy = abs(y1-y2)
+        dx = abs(x1-x2)
+        steep = dy > dx
+        
+        # x1 = int(round((x1+1) * self.width / 2))
+        # y1 = int(round((y1+1) * self.height / 2))
+        # x2 = int(round((x2+1) * self.width / 2))
+        # y2 = int(round((y2+1) * self.height / 2))
+        # steep=(abs(y2 - y1))>(abs(x2 - x1))
+        
         if steep:
             x1, y1 = y1, x1
             x2, y2 = y2, x2
@@ -156,10 +167,52 @@ class Render(object):
                 y2 = (v2[1] + translate[1]) * scale[1]
 
                 self.glLine(x1, y1, x2, y2)
+  
+  def triangle (A,B,C, col):
+    r.current_color = col
+    r.glLine(A,B)
+    r.glLine(B,C)
+    r.glLine(C,A)
     
-r = Render(400,400)
-r.glCreateWindow(400, 400)
-r.glViewPort(400,400 , 400, 400)
-r.glObjModel('silla.obj', (0, 0), (0.3, 0.3))
+    if A.y > B.y:
+      A,B = B,A
+    if A.y > C.y:
+      A,C = C,A
+    if B.y > C.y:
+      B,C = C,B
+    
+    r.current_color = color(0,0,255)
+    
+    dx_ac = C.x - A.x 
+    dy_ac = C.y - A.y
+    
+    mi_ac = dx_ac / dy_ac
+    
+    dx_ab = B.x - A.x 
+    dy_ab = B.y - A.y
+    
+    mi_ab = dx_ab / dy_ab
+    
+    for y in range(A.y,B.y + 1):
+      xi = round(A.x - mi_ac * (A.y - y))
+      xf = round(A.x - mi_ab * (A.y - y))
+      
+      for x in range(xi,xf+1):
+        r.point(x,y)
+      
+    
+r = Render(300,300)
+r.glCreateWindow(300, 300)
+# r.glViewPort(500,500 , 500, 500)
+r.glLine(V3(10,70),V3(50,160))
+r.glLine(V3(50,160), V3(70,80))
+r.glLine(V3(70,80),V3(10,70))
+r.glLine( V3(180,50), V3(150,1) ) 
+r.glLine( V3(150,1), V3(70,180) )
+r.glLine( V3(70,180), V3(180,50) )
+r.glLine( V3(180,150), V3(120,160) )
+r.glLine( V3(120,160), V3(130,180) )
+r.glLine( V3(130,180), V3(180,150) )
+# r.glObjModel('silla.obj', (0, 0), (0.3, 0.3))
 
-r.glFinish("obj.bmp")
+r.glFinish("a.bmp")
